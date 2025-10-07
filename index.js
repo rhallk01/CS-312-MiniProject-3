cc3e88643195cbed50a6d0de6688f5b21d0dce9d
Haley Kloss <hrk85@nau.edu>
2025-10-07 11:50:54 -0700

    added comments

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 3000;
 app.engine("ejs", ejs.__express);
 
-//connect to database
+//connect to database [NEW]
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
@@ -40,7 +40,11 @@ app.use(methodOverride(function (req, res) {
 //set up tags variables to be used later
 var tags = ["all" ,"tech", "lifestyle", "local", "diy", "art", "gardening", "sports"];
 
-//function to get posts 
+//set up variables to track the current user [NEW]
+let currentUserId;
+let currentUserName; 
+
+//function to get posts [NEW]
 async function getPosts() {
   const result = await db.query("SELECT * FROM blogs");
   const posts = result.rows.map((post) => ({
@@ -55,8 +59,6 @@ async function getPosts() {
   }));
   return posts;
 }
-let currentUserId;
-let currentUserName; 
 
 //standard home page render, send blog post, tags list, and current page
 app.get("/", async (req, res) => {
@@ -65,26 +67,31 @@ app.get("/", async (req, res) => {
   res.render("index.ejs", {blogPosts: blogPosts, tags:tags, currentPage: 'index'});
 });
 
-//render login page
+//render login page [NEW]
 app.get("/login", (req, res) => {
   res.render("login.ejs", { error: '' });
 });
 
-//render registration page
+//render registration page [NEW]
 app.get("/register", (req, res) => {
   res.render("register.ejs", { error: '' });
 });
 
+//handle submitted register request [NEW]
 app.post("/register", async (req, res) => {
+  //get the username, userid, and password that was entered
   const userName = req.body.username;
   const userId = req.body.user_id;
   const password = req.body.password;
 
+  //get all instances of rows in the database with the user id that was entered
+  //this is to make sure that it doesn't already exist, as it must be unique
   try {
     const checkResult = await db.query("SELECT * FROM users WHERE user_id = $1", [
       userId,
     ]);
 
+    //if there aren't zero results, render exists message
     if (checkResult.rows.length > 0) {
       res.render("register.ejs", { error: "User ID already exists. Try logging in" });
     } else {
@@ -92,38 +99,51 @@ app.post("/register", async (req, res) => {
         "INSERT INTO users (user_id, password, name) VALUES ($1, $2, $3)",
         [userId, password, userName]
       );
+      //set the current user to the new user and go back to home
       currentUserId = userId;
       currentUserName = userName; 
       return res.redirect("/");
     }
   } catch (err) {
+    //error
     console.log(err);
   }
 });
 
+//handle submitted login request [NEW]
 app.post("/login", async (req, res) => {
+  //get the userid and password that was entered
   const userId = req.body.user_id;
   const password = req.body.password;
 
+  //get all instances of rows in the database with the user id that was entered
+  //make sure this user id exists and that the passwords match
   try {
     const result = await db.query("SELECT * FROM users WHERE user_id = $1", [
       userId,
     ]);
+    //if returns 1, exists! 
     if (result.rows.length > 0) {
+      //get the row and store the password for checking
       const user = result.rows[0];
       const storedPassword = user.password;
 
+      //check if entered password matches db password
       if (password === storedPassword) {
+        //if so, store current user and redirect home
         currentUserId = user.user_id;
         currentUserName = user.name;
         res.redirect("/");
       } else {
+        //if incorrect password, show message on login page
         res.render("login.ejs", { error: "Incorrect password" });
       }
     } else {
+      //if user id doesnt exist, show message on login page
       res.render("login.ejs", { error: "User not found" });
     }
   } catch (err) {
+    //catch error
     console.log(err);
   }
 });
@@ -147,7 +167,7 @@ app.post('/submitPost', async (req, res) => {
     const content = req.body.content;
     const tagName = req.body.tagName.toLowerCase();
 
-    //add post to DB
+    //add post to DB [NEW]
     const result = await db.query(
       "INSERT INTO blogs (creator_name, creator_user_id, title, body, date_created, time_updated, tag ) VALUES ($1, $2, $3, $4, NOW(), NOW(), $5);",
       [creatorName, creatorID, blogTitle, content, tagName]
@@ -160,15 +180,20 @@ app.post('/submitPost', async (req, res) => {
 //go to the edit page with a particular post
 app.get("/edit/:id", async (req, res) => {
     var post = {};
-    //find the original post by id
+    //find the original post by id [NEW]
     const result = await db.query("SELECT * FROM blogs WHERE blog_id = $1", [
       req.params.id,
     ]);
+    //check if post found
     if (result.rows.length > 0) {
+      //get the row
       const gotPost = result.rows[0];
+      //check that the current user is the poster
       if (gotPost.creator_user_id !== currentUserId){
+        //if not, redirect home
         return res.redirect('/');
       }
+      //if so, set variables for rendering edit page with filled in form
       post = {name: gotPost.creator_name, title: gotPost.title, content: gotPost.body, time: gotPost.time_updated, initTime: gotPost.date_created, id: gotPost.blog_id, tag: gotPost.tag};
     } else{
       return res.redirect('/');
@@ -180,17 +205,17 @@ app.get("/edit/:id", async (req, res) => {
 //submit edits to a post, then go redirect to home
 app.post('/edit-form/:id', async (req, res) => {
     var post;
-    //find the original post by id
+    //find the original post by id [NEW]
     const result = await db.query("SELECT * FROM blogs WHERE blog_id = $1", [
       req.params.id,
     ]);
-    if (result.rows.length > 0) {
-      post = result.rows[0];
-    }
-    //if the post is found, update it
+    //get post
+    post = result.rows[0];
+
+    //update it post
     if(post)
     {
-        //get updated title content and tag
+        //get updated title content and tag from submission
         var id = req.params.id;
         var title = req.body.blogTitle;
         var content = req.body.content;
@@ -229,10 +254,9 @@ app.post("/tagSort", (req, res) => {
 
 //if the delete button on a post is clicked, delete it and redirect home
 app.delete('/delete', async (req, res) => {
-    //get id number of post to delete from param
-    //and remove it from blogPosts with filter
+    //get id number of post to delete 
     const idNum = parseInt(req.body.id);
-    //delete post from DB
+    //delete post from DB [NEW]
     await db.query(
       "DELETE FROM blogs \
       WHERE blog_id = $1;",
